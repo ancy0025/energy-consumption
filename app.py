4ec80e2062c3226b69103ed8b4552ec976584293
ancy0025 <anuragcho25@gmail.com>
2025-04-03 14:21:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.write("This is a simple visualization of energy consumption.")
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("results/energy_consumption.csv")
+df = pd.read_csv("energy_consumption.csv")
 
 st.bar_chart(df.set_index("Node"))
